73d342e845fd0f99e84aea92aa7e74e214879f65
Jasmine Yuen <jyuen30@stuy.edu>
2023-11-17 23:05:36 -0800

    figuring out csv and dictionaries

--- app/data/db.py
+++ app/data/db.py
@@ -9,26 +9,28 @@ from db import *
 # sector_list = get_column("sector")
 # sens_list = get_column("data sensitivity")
 
-def model_specified(colArr):
+def model_specified(colArr, recordMin):
     dataDict = {}
-    i = 0
     with open('data_breaches.csv') as f:
         r = csv.DictReader(f)
         for row in r:
-            if (i < 10):
-                # setting empty array of row elements so we can add specific cols
-                rowElements = []
-                orgName = row["organisation"]
-                if (orgName not in dataDict):
-                    # only if not already in table and over certain range for records lost
-                    # new entry into dictionary
-                    dataDict[orgName] = []
-                for colName in colArr:
-                    rowElements.append(row[colName])
-                dataDict[orgName].append(rowElements)
-            i+=1
+            if (row["date"]!=''):
+                recordNum = int(row["records lost"].replace(",",""))
+                yearNum = int(row["year   "])
+                # only adding values with these conditions
+                if (yearNum>=2022 and recordNum>=recordMin):
+                    # setting empty array of row elements so we can add specific cols
+                    rowElements = []
+                    orgName = row["organisation"]
+                    if (orgName not in dataDict):
+                        # only if not already in table and over certain range for records lost
+                        # new entry into dictionary
+                        dataDict[orgName] = []
+                    for colName in colArr:
+                        rowElements.append(row[colName])
+                    dataDict[orgName].append(rowElements)
         return dataDict
 
-print(model_specified(["records lost", "date"]))
+print(model_specified(["records lost", "date", "year   "], 1000000))
 
                     
