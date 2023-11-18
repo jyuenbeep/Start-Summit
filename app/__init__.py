73985b7118ac670de3eaea1fe45e43ac8a3cbcbc
Jasmine Yuen <jyuen30@stuy.edu>
2023-11-18 15:05:33 -0800

    fjdlkajf

--- app/__init__.py
+++ app/__init__.py
@@ -29,30 +29,43 @@ html_template = """
         <script src="../static/js/slider.js" defer> </script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
-    <body>
+    <body style="background-color:#f0edda">
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Trends of Vulnerable Data in Big Corporations</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/stats">Statistics</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/quiz">Quiz</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/aboutus">About Us</a>
-                </li>
-            </ul>
-        </div>
-        </nav>
+      <a class="navbar-brand" href="/"><strong>SECUREHUB</strong></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">HOME <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/stats">STATISTICS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/resources">RESOURCES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/password">PASSWORDS</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/quiz">QUIZ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/game">GAME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/aboutus">ABOUT US</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+    </nav>
         </div>
         <div>
             <image src="static/my_plot.png" class="img-fluid"></image>
