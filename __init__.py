52c472c39d3e6c1582a935a5583545a2a7ebec6e
Jasmine Yuen <jyuen30@stuy.edu>
2023-11-17 18:27:35 -0800

    starting flask app routing

--- __init__.py
+++ __init__.py
@@ -7,4 +7,11 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "23bd2dcea35c795e204d397157f3d55bf1afda7db6519a46f9d1e5a5f02ed45b"
 
+# FLASK APP ROUTING ==================================================================
+
+@app.route("/", methods=['POST'])
+def homepage():
+    return render_template('home.html')
+
+
 
