3b4d9ea1e9b15537ca591c88773abaa1e833dc1d
Jasmine Yuen <jyuen30@stuy.edu>
2023-11-17 18:22:08 -0800

    starting templates

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = "23bd2dcea35c795e204d397157f3d55bf1afda7db6519a46f9d1e5a5f02ed45b"
+
+
