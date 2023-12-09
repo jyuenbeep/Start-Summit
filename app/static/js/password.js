64a47bbdd1a27e35a44541bd7cf134feba8bcdf5
Jasmine Yuen <jyuen30@stuy.edu>
2023-12-08 19:16:45 -0800

    passwords done

---
+++ app/static/js/password.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("copyText");
+
+    /* Create a temporary textarea to copy the text */
+    var tempTextarea = document.createElement("textarea");
+    tempTextarea.value = copyText.innerText;
+
+    /* Select the text inside the temporary textarea */
+    tempTextarea.select();
+    tempTextarea.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the temporary textarea to the clipboard */
+    document.execCommand("copy");
+  }
+
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    var scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
